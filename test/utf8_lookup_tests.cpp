b8ac4ee656be38a9b94a427a2cc629f9e9003e61
wc-duck <Fredrik.Kihlander@gmail.com>
2013-05-16 21:47:46 +0200

    added unittest for chars from all octets

--- test/utf8_lookup_tests.cpp
+++ test/utf8_lookup_tests.cpp
@@ -349,7 +349,6 @@ TEST( utf8, octet_2_and_3 )
 	EXPECT_EQ( 0u, res[11].offset );
 }
 
-
 TEST( utf8, octet_1_2_and_3 )
 {
 	unsigned int test_cps[] = { 'A', 'B', 'b', 'd',
@@ -415,6 +414,85 @@ TEST( utf8, octet_1_2_and_3 )
 	EXPECT_EQ( 0u, res[16].offset );
 }
 
+TEST( utf8, octet_1_2_3_and_4 )
+{
+	unsigned int test_cps[] = { 'A', 'B', 'b', 'd',
+								130,     // offset 5 = (130)
+								165,     // offset 6 = ¥
+								228,     // offset 7 = ä
+								229,     // offset 8 = å
+								246,     // offset 9 = ö
+								0x7CF,   // offset 10
+								0x7FF,   // offset 11 = max octet 2
+
+								// octet 3
+								0x800,
+								0x1024,
+								0x1025,
+								0xFFFF,
+
+								 // octet 4
+								0x10000,
+								0x10801,
+								0x10802,
+								0x10FFFF };
+
+	uint8_t table[ 512 ];
+	pack_table( table, sizeof(table), test_cps, ARRAY_LENGTH(test_cps) );
+
+	const uint8_t* str = (const uint8_t*)"dbBA"
+									     "q"         // q ... do not exist in table ...
+									     "\xc3\xa5"  // å
+									     "\xc3\xa4"  // ä
+									     "\xc3\xb6"  // ö
+									     "\xc2\xa5"  // ¥
+									     "\xdf\x8f"
+									     "\xdf\xbf"  // max octet 2
+									     "\xc2\xa7"  // § ... do not exist in table ...
+									     "\xe0\xa0\x80"  // 0x800
+									     "\xe1\x80\xa4"  // 0x1024
+									     "\xe1\x80\xa5"  // 0x1025
+									     "\xef\xbf\xbf"  // 0xFFFF
+									     "\xe2\x81\x88"  // ... do not exist in table ...
+										 "\xf0\x90\x80\x80"  // 0x10000
+										 "\xf0\x90\xa0\x81"  // 0x10801
+										 "\xf0\x90\xa0\x82"  // 0x10802
+										 "\xf4\x8f\xbf\xbf"  // 0x10FFF
+										 "\xf0\x90\xa0\x83";
+	const uint8_t* str_iter = str;
+
+	utf8_lookup_result res[128];
+	size_t res_size = ARRAY_LENGTH( res );
+
+	EXPECT_EQ( UTF8_LOOKUP_ERROR_OK, utf8_lookup_perform( table, str, &str_iter, res, &res_size ) );
+	EXPECT_EQ( 22u, res_size );
+
+	EXPECT_EQ(  4u, res[0].offset );
+	EXPECT_EQ(  3u, res[1].offset );
+	EXPECT_EQ(  2u, res[2].offset );
+	EXPECT_EQ(  1u, res[3].offset );
+	EXPECT_EQ(  0u, res[4].offset ); // do not exist
+
+	EXPECT_EQ(  8u, res[5].offset );
+	EXPECT_EQ(  7u, res[6].offset );
+	EXPECT_EQ(  9u, res[7].offset );
+	EXPECT_EQ(  6u, res[8].offset );
+	EXPECT_EQ( 10u, res[9].offset );
+	EXPECT_EQ( 11u, res[10].offset );
+	EXPECT_EQ(  0u, res[11].offset ); // do not exist
+
+	EXPECT_EQ( 12u, res[12].offset );
+	EXPECT_EQ( 13u, res[13].offset );
+	EXPECT_EQ( 14u, res[14].offset );
+	EXPECT_EQ( 15u, res[15].offset );
+	EXPECT_EQ( 0u,  res[16].offset ); // do not exist
+
+	EXPECT_EQ( 16u, res[17].offset );
+	EXPECT_EQ( 17u, res[18].offset );
+	EXPECT_EQ( 18u, res[19].offset );
+	EXPECT_EQ( 19u, res[20].offset );
+	EXPECT_EQ( 0u,  res[21].offset ); // do not exist
+}
 
 int main( int argc, char** argv )
 {
